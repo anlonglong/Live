05f1cd3fdadf034656e7aa96c0279d572b0e7bb8
anlonglong <940752944@qq.com>
2017-12-23 13:14:19 +0800

    删除app

--- app/src/main/java/com/team/zhuoke/ui/refreshview/XRefreshViewState.java
+++
@@ -1,5 +0,0 @@
-package com.team.zhuoke.ui.refreshview;
-
-public enum XRefreshViewState {
-	STATE_READY, STATE_REFRESHING, STATE_NORMAL, STATE_LOADING, STATE_COMPLETE,STATE_FINISHED,STATE_RELEASE_TO_LOADMORE
-}
